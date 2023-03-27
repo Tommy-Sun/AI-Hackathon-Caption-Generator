31fc00aceee007a33ac2a0a19c8fbe7cf9040176
Tommy Kebschull <tkebschull@chatmeter.com>
2023-03-27 12:05:18 -0700

    AI-Hackathon-Caption-Generator-V1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CaptionForm from "./CaptionForm";
+
+export enum CaptionTypes {
+  NEWOFFER = "New Offer",
+  NEWEVENT = "New Event",
+  OURVALUES = "Our Values As An Organisation",
+}
 
 function App() {
+  const [captionTypeSelected, setCaptionTypeSelected] = useState(
+    CaptionTypes.NEWOFFER
+  );
+  console.log(captionTypeSelected);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2 className="header">What Kind Of Caption Are You Posting?</h2>
+      <div className="container">
+        <select
+          onChange={(event) =>
+            setCaptionTypeSelected(event.target.value as CaptionTypes)
+          }
         >
-          Learn React
-        </a>
-      </header>
+          {Object.values(CaptionTypes).map((captionType, index) => (
+            <option key={index}>{captionType}</option>
+          ))}
+        </select>
+      </div>
+      <div className="container">
+        <CaptionForm captionType={captionTypeSelected} />
+      </div>
     </div>
   );
 }
